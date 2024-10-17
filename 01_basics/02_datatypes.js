c4f73ff89c44c2f2c69acf7a858b30ee68a535bb
Am24an <jaiakhkumar@gmail.com>
2024-10-17 16:21:12 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // Will treat all the JS code as newer version
+
+//alert(3 + 3) -> We are using nodejs, not the browser 
+
+// code readability should be high.
+
+console.log("Aman")
+
+let name = "nature"
+let age = 22
+let isLoggedIn = false
+
+// Number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => used for unique
+
+// object
+console.log(typeof name);   //string
+console.log(typeof null);  //object
+console.log(typeof age);   // number
+console.log(typeof undefined); //undefined (it is type)
+
+
+
--- 01_basics/02_variables.js
+++
@@ -1,3 +0,0 @@
-"use strict"; // Will treat all the JS code as newer version
-
-//alert(3 + 3) // We are using nodejs, not the browser 
